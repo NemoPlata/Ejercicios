d7612370f53878fda98ffe06b5a373719d526e13
carlogilmar <carlogilmar12@gmail.com>
2019-02-03 21:43:46 -0600

    Adding correct class

--- exercise2.py
+++ exercise2.py
@@ -1,16 +1,9 @@
 class Park:
-	 def __init__(self, price):
-		 self.costs = price
+     def __init__(self, price):
+         self.costs = price
 
-	 def park_costs( self ):
-	 	print ("Ingresa la edad de la persona: ")
-	 	age = int(input())
-	   if(10>age):
-	   	 return self.costs + (self.costs * 0.25)
-	   else:
-	   	 return self.costs
-
-	 def show_data (self):
-	 	print(":: Estos son los datos ::")
-	 	print("La edad de la persona es: ", age)
-		print("El costo del juego es de: ", self.park_costs())
+     def park_costs( self, age ):
+       if( 10 < age ):
+         return self.costs + (self.costs * 0.25)
+       else:
+         return self.costs
