146b38ea3ce20472c562f22eea7467446c8446b9
NemoPlata <46588931+NemoPlata@users.noreply.github.com>
2019-02-03 19:14:39 -0600

    Add files via upload

---
+++ Mainexercise2.py
@@ -0,0 +1,7 @@
+from exercise2 import Park
+
+print ("Bienvenido!!")
+print ("Ingresa la edad del visitante: ")
+years = int(input())
+visitor = Park(years)
+visitor.show_data()
---
+++ exercise2.py
@@ -0,0 +1,15 @@
+class Park:
+	 def __init__(self, age):
+		 self.age = years
+		 int costs = 50
+
+	 def park_costs ( self ):
+	   if( self.age < 10 ):
+	   	costs = costs * 0.25
+		return costs
+	   else:
+		return costs
+
+	 def show_data ( self ):
+	 	print ("La edad de la persona es: ",self.show_data())
+		print("El costo del juego es de: ",costs)
